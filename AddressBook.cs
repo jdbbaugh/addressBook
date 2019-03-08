b494c691fc640cb8e1978090bf2a4ab6eb9ab307
jdbbaugh <jbaugh78@msn.com>
2019-03-08 11:00:41 -0600

    some progress on initial file layout

---
+++ AddressBook.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+public class AddressBook
+{
+  public Dictionary<string, Contact> Person {get; set;} = new Dictionary<string, Contact>();
+  public void AddContact(Contact contact)
+  {
+
+  }
+}
